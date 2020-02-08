bd91c3b521dfc2a496f8f8fe4cd5c4e973597b9a
uditgulati <uditgulati0@gmail.com>
2020-02-08 14:23:12 +0530

    add plot_1

---
+++ plot_1.R
@@ -0,0 +1,54 @@
+library(MPCC)
+library(MPCCmkl)
+library(rbenchmark)
+cols <- c("test", "replications", "elapsed", "relative")
+reps <- 20
+set.seed(1)
+
+sizes = c(100, 500, 1000, 2000, 4000, 8000)
+cor_vals = c()
+mkl_vals = c()
+PCC_vals = c()
+naive_vals = c()
+
+for (i in 1:length(sizes)) {
+  m <- sizes[i]
+  n <- sizes[i]
+  x <- matrix(rnorm(m*n), m, n)
+  y <- matrix(rnorm(m*n), m, n)
+
+  a = benchmark(cor(x, y), replications=reps, columns=cols)
+  b = benchmark(MPCCmkl::PCC(x, y), replications=reps, columns=cols)
+  c = benchmark(MPCC::PCC(x, y), replications=reps, columns=cols)
+  d = benchmark(PCC.naive(x, y), replications=reps, columns=cols)
+
+  cor_vals[i] = a[1, 3]
+  mkl_vals[i] = b[1, 3]
+  PCC_vals[i] = c[1, 3]
+  naive_vals[i] = d[1, 3]
+}
+
+png("plot_1.png", width=1024, height = 800)
+ 
+# Make a basic graph
+plot( cor_vals~sizes , type="b" , bty="l" , xlab="Matrix size" , ylab="Time(seconds)" , col=rgb(0.9,0.1,0.1,0.7) , lwd=3 , pch=17)
+lines( mkl_vals~sizes , col=rgb(0.1,0.9,0.1,0.7) , lwd=3 , pch=18 , type="b" )
+lines( PCC_vals~sizes , col=rgb(0.1,0.1,0.9,0.7) , lwd=3 , pch=19 , type="b" )
+lines( naive_vals~sizes , col=rgb(0.3,0.3,0.3,0.7) , lwd=3 , pch=20 , type="b" )
+ 
+# Add a legend
+legend("bottomleft", 
+  legend = c("cor", "MKL", "OpenBLAS", "naive"), 
+  col = c(rgb(0.9,0.1,0.1,0.7), 
+  rgb(0.1,0.9,0.1,0.7),
+  rgb(0.1,0.1,0.9,0.7),
+  rgb(0.3,0.3,0.3,0.7)), 
+  pch = c(17, 18, 19, 20), 
+  bty = "n", 
+  pt.cex = 2, 
+  cex = 1.2, 
+  text.col = "black", 
+  horiz = F , 
+  inset = c(0.1, 0.1, 0.1, 0.1))
+
+dev.off()
