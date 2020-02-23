bb5055a3a81af1464a62645236a75362990c6399
uditgulati <uditgulati0@gmail.com>
2020-02-23 20:54:42 +0530

    add script, no loop coz too lazy

---
+++ bench.sh
@@ -0,0 +1,64 @@
+# plot 1
+
+Rscript plot_1.R data_1_0
+Rscript plot_1.R data_1_1
+Rscript plot_1.R data_1_2
+Rscript plot_1.R data_1_3
+Rscript plot_1.R data_1_4
+Rscript plot_1.R data_1_5
+Rscript plot_1.R data_1_6
+Rscript plot_1.R data_1_7
+Rscript plot_1.R data_1_8
+Rscript plot_1.R data_1_9
+
+# plot 2
+
+Rscript plot_2.R data_2_0
+Rscript plot_2.R data_2_1
+Rscript plot_2.R data_2_2
+Rscript plot_2.R data_2_3
+Rscript plot_2.R data_2_4
+Rscript plot_2.R data_2_5
+Rscript plot_2.R data_2_6
+Rscript plot_2.R data_2_7
+Rscript plot_2.R data_2_8
+Rscript plot_2.R data_2_9
+
+# plot 3
+
+Rscript plot_3.R data_3_0
+Rscript plot_3.R data_3_1
+Rscript plot_3.R data_3_2
+Rscript plot_3.R data_3_3
+Rscript plot_3.R data_3_4
+Rscript plot_3.R data_3_5
+Rscript plot_3.R data_3_6
+Rscript plot_3.R data_3_7
+Rscript plot_3.R data_3_8
+Rscript plot_3.R data_3_9
+
+# plot rec 1
+
+Rscript plot_rec_1.R data_rec_1_0
+Rscript plot_rec_1.R data_rec_1_1
+Rscript plot_rec_1.R data_rec_1_2
+Rscript plot_rec_1.R data_rec_1_3
+Rscript plot_rec_1.R data_rec_1_4
+Rscript plot_rec_1.R data_rec_1_5
+Rscript plot_rec_1.R data_rec_1_6
+Rscript plot_rec_1.R data_rec_1_7
+Rscript plot_rec_1.R data_rec_1_8
+Rscript plot_rec_1.R data_rec_1_9
+
+# plot rec 2
+
+Rscript plot_rec_2.R data_rec_2_0
+Rscript plot_rec_2.R data_rec_2_1
+Rscript plot_rec_2.R data_rec_2_2
+Rscript plot_rec_2.R data_rec_2_3
+Rscript plot_rec_2.R data_rec_2_4
+Rscript plot_rec_2.R data_rec_2_5
+Rscript plot_rec_2.R data_rec_2_6
+Rscript plot_rec_2.R data_rec_2_7
+Rscript plot_rec_2.R data_rec_2_8
+Rscript plot_rec_2.R data_rec_2_9
